c76ff6f96d3e023498fdfadf249c51e05361c506
Nakib Iqbal <nakibiqbal.2006@gmail.com>
2025-03-15 15:56:51 +0600

    Finally fixed the problem of showing the output different in vercel deployment and localhost.

--- src/pages/Section4/Section4.jsx
+++ src/pages/Section4/Section4.jsx
@@ -1,9 +1,8 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import "./section4.css";
 import "./images.css";
 import "./text.css";
 import { useRef } from "react";
-// import Images from "./Images";
-// import Text from "./Text";
 import { Data } from "./data";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { useState } from "react";
@@ -13,12 +12,9 @@ const Section4 = () => {
   const ref4 = useRef(null);
   const [data] = useState(Data);
   const aboutMe = `I'm a self-taught passionate Frontend Developer and I'm a positive dreamer. I cherish a long dream in my heart for doing something better/creative in this sector. InshaAllah (if Allah wills). I'll be able to make better by having codial supports of all well-wishers. I love both programming and designing. I'm also interested in open source, and creative coding projects.`;
-
   return (
     <section ref={ref4} id="section4">
       <div className="sec4Wrapper">
-        {/* <Images ref4={ref4} />
-        <Text ref4={ref4} /> */}
         {data.map(({ id, src, susu }) => {
           const { scrollYProgress } = useScroll({ target: ref4, offset: [`${id * 3}% ${id * 3}%`, "300% 300%"] });
           const Scale = (start, end) => useTransform(scrollYProgress, [0, 1], [start, end]);
