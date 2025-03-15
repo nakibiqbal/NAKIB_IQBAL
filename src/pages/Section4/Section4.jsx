2d56176f8704fb6837d57171660f85589cadf113
Nakib Iqbal <nakibiqbal.2006@gmail.com>
2025-03-15 22:32:47 +0600

    Mouse interaction effect added to Section 4

--- src/pages/Section4/Section4.jsx
+++ src/pages/Section4/Section4.jsx
@@ -7,13 +7,35 @@ import { Data } from "./data";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { useState } from "react";
 
-
 const Section4 = () => {
   const ref4 = useRef(null);
   const [data] = useState(Data);
   const aboutMe = `I'm a self-taught passionate Frontend Developer and I'm a positive dreamer. I cherish a long dream in my heart for doing something better/creative in this sector. InshaAllah (if Allah wills). I'll be able to make better by having codial supports of all well-wishers. I love both programming and designing. I'm also interested in open source, and creative coding projects.`;
+  const handleMouseMoveSection = (e) => {
+    const { currentTarget } = e; // Current section
+    const rect = currentTarget.getBoundingClientRect();
+    const x = e.clientX - rect.left
+    const y = e.clientY - rect.top
+
+    // Elements within the current section
+    const elements = currentTarget.querySelectorAll(".stick4 img");
+
+    elements.forEach((el, i) => {
+      const moveX = (x / rect.width - 0.5) * (i % 2 === 0 ? 50 : -50); // Cursor-relative movement
+      const moveY = (y / rect.height - 0.5) * (i % 2 === 0 ? 100 : -100);
+
+      el.style.transition = "transform 0.8s cubic-bezier(0.22, 1, 0.36, 1)";
+      el.style.transform = `translate(${moveX}px, ${moveY}px)`;
+    });
+  };
+  const handleMouseLeaveSection = (e) => {
+    const elements = e.currentTarget.querySelectorAll(".stick4 img");
+    elements.forEach((el) => {
+      el.style.transform = "translate(0, 0)";
+    });
+  };
   return (
-    <section ref={ref4} id="section4">
+    <section onMouseMove={handleMouseMoveSection} onMouseLeave={handleMouseLeaveSection} ref={ref4} id="section4">
       <div className="sec4Wrapper">
         {data.map(({ id, src, susu }) => {
           const { scrollYProgress } = useScroll({ target: ref4, offset: [`${id * 3}% ${id * 3}%`, "300% 300%"] });
