801e131d11e96baedbef723a30541ccc038e75cf
Nakib Iqbal <nakibiqbal.2006@gmail.com>
2025-03-21 15:08:31 +0600

    Section4 fixed

--- src/pages/Section4/Images.jsx
+++ src/pages/Section4/Images.jsx
@@ -1,33 +1,37 @@
-/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable react/prop-types */
 import "./images.css";
 import { Data } from "./data";
 import { useState } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
-const Images = ({ ref4 }) => {
-    const [data] = useState(Data);
 
+export default function Images({ ref4 }) {
+    const [data] = useState(Data);
     return (
         <div className="parentImgDiv">
-            {data.map(({ id, src, susu }, index) => {
-                const { scrollYProgress } = useScroll({ target: ref4, offset: [`${index * 3}% 100%`, `180% 0%`] });
-                const z = useTransform(scrollYProgress, [0, 1], [-200, 1700]);
-                const scale = useTransform(scrollYProgress, [0, 1], [0, 1]);
-                return (
-                    <motion.div key={id}
-                        className="test"
-                        style={{
-                            z,
-                            scale
-                        }}
-                    >
-                        <div className={`stick4 ${susu}`}>
-                            <img src={src} loading="lazy" />
-                        </div>
-                    </motion.div>
-                );
-            })}
+            <>
+                {data.map(({ id, src, susu }) => {
+                    return <ScrollFunction key={id} id={id} ref4={ref4} src={src} susu={susu} />;
+                })}
+            </>
         </div>
     )
 }
+const ScrollFunction = ({ id, ref4, src, susu }) => {
+    const { scrollYProgress } = useScroll({ target: ref4, offset: [`${id * 3}% 100%`, "600% 0%"] });
+    const z = useTransform(scrollYProgress, [0, 1], [100, 2000]);
+    const scale = useTransform(scrollYProgress, [0, 1], [0, 1]);
+    const rotate = useTransform(scrollYProgress, [0, 1], [-360, 360]);
+    return <motion.div
+        className="test"
+        style={{
+            z,
+            scale,
+            rotate,
+        }}
+    >
+        <div className={`stick4 ${susu}`}>
+            <img src={src} loading="lazy" />
+        </div>
+    </motion.div>
 
-export default Images
+}
--- src/pages/Section4/Section4.jsx
+++ src/pages/Section4/Section4.jsx
@@ -1,14 +1,12 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 import "./section4.css";
 import "./images.css";
 import "./text.css";
 import { useRef } from "react";
-import { motion, useScroll, useTransform } from "framer-motion";
 import Images from "./Images";
+import Text from "./Text";
 
 const Section4 = () => {
   const ref4 = useRef(null);
-  const aboutMe = `I'm a self-taught passionate Frontend Developer and I'm a positive dreamer. I cherish a long dream in my heart for doing something better/creative in this sector. InshaAllah (if Allah wills). I'll be able to make better by having codial supports of all well-wishers. I love both programming and designing. I'm also interested in open source, and creative coding projects.`;
   // const handleMouseMoveSection = (e) => {
   //   const { currentTarget } = e; // Current section
   //   const rect = currentTarget.getBoundingClientRect();
@@ -36,27 +34,9 @@ const Section4 = () => {
     // <section onMouseMove={handleMouseMoveSection} onMouseLeave={handleMouseLeaveSection} ref={ref4} id="section4">
     <section ref={ref4} id="section4">
       <div className="sec4Wrapper">
-
         <Images ref4={ref4} />
 
-        <div className="aboutMe">
-          <div className="mainTxt">
-            {
-              aboutMe.split(" ").map((word, index) => {
-                const { scrollYProgress } = useScroll({ target: ref4, offset: ["0% 0%", "100% 100%"] });
-                const WordOpacity = (index) => useTransform(scrollYProgress, [index * 0.016, (index + 1) * 0.016], [0, 1])
-                return (
-                  <motion.h1
-                    style={{ opacity: WordOpacity(index) }}
-                    key={index}>
-                    {word}
-                  </motion.h1>
-                );
-              })
-            }
-          </div>
-        </div>
-
+        <Text ref4={ref4} />
       </div>
     </section>
   );
