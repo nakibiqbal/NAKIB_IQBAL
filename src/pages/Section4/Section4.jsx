6779c094f98e5d968dff273714b1bf53c2e95e10
Nakib Iqbal <nakibiqbal.2006@gmail.com>
2025-03-22 03:01:57 +0600

    Attempt-46

--- src/pages/Section4/Section4.jsx
+++ src/pages/Section4/Section4.jsx
@@ -35,7 +35,7 @@ const Section4 = () => {
     // <section onMouseMove={handleMouseMoveSection} onMouseLeave={handleMouseLeaveSection} ref={ref4} id="section4">
     <section ref={ref4} id="section4">
       <div className="sec4Wrapper">
-        <Images ref4={ref4.current} />
+        <Images ref4={ref4} />
 
         {/* <Text ref4={ref4} /> */}
       </div>
