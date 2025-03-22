0a68382184d68e9cb7573ecd2e2d5fbc3597b8df
Nakib Iqbal <nakibiqbal.2006@gmail.com>
2025-03-22 13:04:02 +0600

    Attempt-74

--- src/pages/Section4/Images.jsx
+++ src/pages/Section4/Images.jsx
@@ -17,7 +17,7 @@ export default function Images({ ref4 }) {
     )
 }
 const ScrollFunction = ({ index, ref4, src, susu }) => {
-    const { scrollYProgress } = useScroll({ target: ref4, offset: [`${(index + 10) * 1.25}% -120%`, "300% 80%"] });
+    const { scrollYProgress } = useScroll({ target: ref4, offset: [`${(index + 10) * 1.25}% -120%`, "300% 120%"] });
     const z = useTransform(scrollYProgress, [0, 1], [100, 2000]);
     const scale = useTransform(scrollYProgress, [0, 1], [0, 1]);
     const rotate = useTransform(scrollYProgress, [0, 1], [-360, 360]);
