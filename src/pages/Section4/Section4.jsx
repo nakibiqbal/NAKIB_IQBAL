ae676be3e8ad13b07ec681614f4d5516966b5271
Nakib Iqbal <nakibiqbal.2006@gmail.com>
2025-03-22 16:09:52 +0600

    Attempt-12

--- src/pages/Section4/Section4.jsx
+++ src/pages/Section4/Section4.jsx
@@ -8,6 +8,7 @@ import { motion, useScroll, useTransform } from "framer-motion";
 const Section4 = () => {
   const ref4 = useRef(null);
   const aboutMe = `I'm a self-taught passionate Frontend Developer and I'm a positive dreamer. I cherish a long dream in my heart for doing something better/creative in this sector. InshaAllah (if Allah wills). I'll be able to make better by having codial supports of all well-wishers. I love both programming and designing. I'm also interested in open source, and creative coding projects.`;
+  const { scrollYProgress } = useScroll({ target: ref4, offset: [`start end`, "end end"], });
 
   return (
     <section ref={ref4} id="section4">
@@ -19,7 +20,13 @@ const Section4 = () => {
           <div className="mainTxt">
             {
               aboutMe.split(" ").map((word, index) => {
-                return <ScrollingText key={index} word={word} index={index} ref4={ref4} />
+                const WordOpacity = () => useTransform(scrollYProgress, [index * 0.020, (index + 1) * 0.020], [0, 1])
+                return <motion.h1
+                  style={{ opacity: WordOpacity() }}
+                  key={index}>
+                  {word}
+                </motion.h1>
+
               })
             }
           </div>
@@ -32,15 +39,3 @@ const Section4 = () => {
 };
 
 export default Section4;
-
-const ScrollingText = ({ word, index, ref4 }) => {
-  const { scrollYProgress } = useScroll({ target: ref4, offset: [`start end`, "end end"], });
-  const wordOpacity = useTransform(scrollYProgress, [index * 0.020, (index + 1) * 0.020], [0, 1])
-  return (
-    <motion.h1
-      style={{ opacity: wordOpacity }}
-      key={index}>
-      {word}
-    </motion.h1>
-  );
-}
