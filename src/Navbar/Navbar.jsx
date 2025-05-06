d3f1126dd3a22bd0828dfc66f5140e106ffe6a3b
Nakib Iqbal <nakibiqbal.2006@gmail.com>
2025-05-06 23:48:14 +0600

    Navbar complete without well stuctured.

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,19 +1,20 @@
 import { useEffect, useState } from "react";
-// import { useScramble } from "use-scramble";
 import { AnimatePresence, motion } from "framer-motion";
+// import { useScramble } from "use-scramble";
 import edit2 from "../assets/edit2.jpg";
 import "./Navbar.css";
+import Items from "./Items";
 
 const Navbar = () => {
+    // const navData = [
+    //     { item: "Skills", section: "#section5", id: 1 },
+    //     { item: "Services", section: "#section6", id: 2 },
+    //     { item: "Projects", section: "#image_reveal", id: 3 },
+    //     { item: "About", section: "#section4", id: 4 },
+    //     { item: "Contact", section: "#ContactSection", id: 5 },
+    // ]
     const [isClick, setIsClick] = useState(false);
 
-    const navData = [
-        { item: "Skills", section: "#section5", id: 1 },
-        { item: "Services", section: "#section6", id: 2 },
-        { item: "Projects", section: "#image_reveal", id: 3 },
-        { item: "About", section: "#section4", id: 4 },
-        { item: "Contact", section: "#ContactSection", id: 5 },
-    ]
 
     // For small screen size
     const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 812);
@@ -50,26 +51,29 @@ const Navbar = () => {
                         onClick={() => setIsClick(!isClick)}>MENU</motion.button>
 
                     <div
-                        style={{
-                            zIndex: isClick ? 1 : -1,
-                            background: isClick ? "#000000a6" : "transparent",
-                        }}
+                        // onClick={() => setIsClick(false)} // Close the menu when clicking outside
                         className="parentNavItem"
+                        style={{
+                            background: isClick ? "#000000a6" : "transparent",
+                            pointerEvents: isClick ? "all" : "none"
+                        }}
                     >
-                        <div
-                            onClick={() => setIsClick(false)} // Close the menu when clicking outside
-                            style={{
-                                position: "absolute",
-                                top: 0,
-                                left: 0,
-                                width: "100%",
-                                height: "100%",
-                            }}
-                        ></div>
+                        {
+                            isClick && <div
+                                onClick={() => setIsClick(false)} // Close the menu when clicking outside
+                                style={{
+                                    position: "absolute",
+                                    top: 0,
+                                    left: 0,
+                                    width: "100%",
+                                    height: "100%",
+                                }}
+                            />
+                        }
                         <motion.nav
                             animate={{
-                                width: isClick ? (isSmallScreen ? "90%" : "48rem") : "7rem",
-                                height: isClick ? (isSmallScreen ? "90%" : "95%") : "3.5rem",
+                                width: isClick ? (isSmallScreen ? "93.5%" : "48rem") : (isSmallScreen ? "6rem" : "7rem"),
+                                height: isClick ? (isSmallScreen ? "92%" : "95%") : (isSmallScreen ? "3.1rem" : "3.5rem"),
                                 border: isClick ? "1px solid #00ff00bf" : "1px dashed #00ff00bf",
                             }}
                             transition={{
@@ -85,10 +89,8 @@ const Navbar = () => {
                                     <div className="navItems">
 
                                         <div className="navItemsChild">
-                                            {navData.map((data, index) => {
-                                                const { item, section, id } = data;
-                                                return <Items key={index} item={item} section={section} id={id} />
-                                            })}
+                                            <Items setIsClick={setIsClick} />
+
                                         </div>
                                         <motion.div
                                             className="nakibIqbal"
@@ -121,38 +123,3 @@ const Navbar = () => {
 }
 
 export default Navbar
-
-
-const Items = ({ item, section, id }) => {
-    // hook returns a ref
-    // const { scrambleRef, replay } = useScramble({
-    //     text: item,
-    //     onStart: () => console.log("start"), // Added onStart callback
-    //     onAnimationFrame: (result) => console.log(result)
-    // });
-    return <motion.a
-        key={id}
-        // ref={scrambleRef}
-        // onMouseOver={replay}
-        // onFocus={replay}
-        initial={{ opacity: 0, scale: 1.4, y: 100, filter: "blur(20px)", color: "green" }}
-        whileHover={{ color: "#00ff00bf" }}
-        animate={{ opacity: 1, scale: 1, y: 0, filter: "blur(0px)", transition: { delay: id * 0.1, ease: [0.87, 0, 0.13, 1], duration: 1 } }}
-        exit={{ opacity: 0, y: 100, scale: 1, filter: "blur(20px)", transition: { ease: [0.87, 0, 0.13, 1], duration: 0.5 } }}
-        transition={{
-            duration: 0.5,
-            ease: [0.87, 0, 0.13, 1]
-        }}
-        href={section}
-        onClick={(e) => {
-            e.preventDefault(); // Prevent the default jump behavior
-            const target = document.querySelector(section);
-            target?.scrollIntoView({
-                behavior: "smooth", // Smooth scroll
-                block: "start",     // Scroll to the top of the section
-            });
-        }}
-    >
-        {item}
-    </motion.a>
-}
