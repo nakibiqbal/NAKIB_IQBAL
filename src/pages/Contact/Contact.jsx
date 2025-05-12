85cafe704cba468f3de69b4149e473e9c392a4e9
Nakib Iqbal <nakibiqbal.2006@gmail.com>
2025-05-12 21:37:20 +0600

    All issue fixed

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,4 +1,5 @@
 import { FaGithub, FaFacebook, FaLinkedin } from 'react-icons/fa';
+import { motion } from "framer-motion"
 import './Contact.css'; // Import the custom CSS file
 import MagnetButton from './MagnetButton';
 import bg5 from "../../assets/bg5.png"
@@ -8,27 +9,38 @@ function Contact() {
     return (
         <section id="ContactSection">
 
-            <h1>Let&apos;s connect.</h1>
+            <div className="contactMe">
+                <h1>Let&apos;s connect.</h1>
 
-            <div className="CLinks">
-
-
-                <div className="mail">
-                    <a href="mailto:nakibiqbal.2006@gmail.com">nakibiqbal.2006@gmail.com</a>
+                <div className="openToWork">
+                    <motion.span
+                        initial={{ boxShadow: "0 0 0 0px green" }}
+                        animate={{ boxShadow: "0 0 0 8px green" }}
+                        transition={{ repeat: Infinity, repeatType: "reverse", duration: 1 }}
+                    ></motion.span>
+                    <p>OPEN TO WORK</p>
                 </div>
 
-                <div className="contactIcons">
-                    <MagnetButton>
-                        <a href="https://www.facebook.com/nakib.iqbal.98" target='_blink' ><FaFacebook /></a>
-                    </MagnetButton>
-                    <MagnetButton>
-                        <a href="https://www.linkedin.com/in/nakib-iqbal/" target='_blink'><FaLinkedin /></a>
-                    </MagnetButton>
-                    <MagnetButton>
-                        <a href="https://github.com/nakibiqbal" target='_blink'><FaGithub /></a>
-                    </MagnetButton>
-                </div>
+                <div className="CLinks">
 
+
+                    <div className="mail">
+                        <a href="mailto:nakibiqbal.2006@gmail.com">nakibiqbal.2006@gmail.com</a>
+                    </div>
+
+                    <div className="contactIcons">
+                        <MagnetButton>
+                            <a href="https://www.facebook.com/nakib.iqbal.98" target='_blink' ><FaFacebook /></a>
+                        </MagnetButton>
+                        <MagnetButton>
+                            <a href="https://www.linkedin.com/in/nakib-iqbal/" target='_blink'><FaLinkedin /></a>
+                        </MagnetButton>
+                        <MagnetButton>
+                            <a href="https://github.com/nakibiqbal" target='_blink'><FaGithub /></a>
+                        </MagnetButton>
+                    </div>
+
+                </div>
             </div>
 
             <div className="islamicQuote">
